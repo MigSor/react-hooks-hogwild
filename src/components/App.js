967ad8381285f15fc5d752444f13485e7add448a
Miguel Soriano <miguel.sor.88@gmail.com>
2023-10-10 17:37:56 +1000

    started the hogs project

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,23 @@ import Nav from "./Nav";
 import hogs from "../porkers_data";
 
 function App() {
-	return (
-		<div className="App">
-			<Nav />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Nav />
+      <div className="ui grid container">
+        {hogs.map((hog, index) => {
+          return (
+            <div className="ui eight wide column " key={index}>
+              <div className="pigTile">
+                <img src={hog.image} alt="" style={{ width: "100%" }} />
+                <h3>{hog.name}</h3>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
