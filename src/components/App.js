a726c5a2ed2cda760f36ee22ecda923f981009c8
Miguel Soriano <miguel.sor.88@gmail.com>
2023-10-12 20:43:33 +1000

    added toggle  details when pigtile is clicked

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,51 @@ import React from "react";
 import Nav from "./Nav";
 
 import hogs from "../porkers_data";
+import { useState } from "react";
 
 function App() {
+  const [showHogDetails, setShowHogDetails] = useState(false);
+  const [chosenHog, setChosenHog] = useState("");
+
+  function showDetails(hog) {
+    console.log(hog);
+    setShowHogDetails(!showHogDetails);
+    setChosenHog(hog.name);
+  }
+
   return (
     <div className="App">
       <Nav />
       <div className="ui grid container">
         {hogs.map((hog, index) => {
           return (
-            <div className="ui eight wide column " key={index}>
+            <div
+              className="ui eight wide column "
+              key={index}
+              onClick={() => {
+                showDetails(hog);
+              }}
+            >
               <div className="pigTile">
                 <img src={hog.image} alt="" style={{ width: "100%" }} />
-                <h3>{hog.name}</h3>
+                <h2>{hog.name}</h2>
+                {showHogDetails && hog.name === chosenHog && (
+                  <div className="hogDetails">
+                    <h3>
+                      Highest Medal Achieved :{" "}
+                      <em>{hog["highest medal achieved"]}</em>
+                    </h3>
+                    <h3>
+                      Specialty : <em>{hog.specialty}</em>
+                    </h3>
+                    <h3>
+                      Greased : <em>{hog.greased}</em>
+                    </h3>
+                    <h3>
+                      Weight : <em>{hog.weight}</em>
+                    </h3>
+                  </div>
+                )}
               </div>
             </div>
           );
